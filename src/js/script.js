d72c78704c101ca4bec0901879b59dda90882f3d
Ilia Filimonov <9991571155i@gmail.com>
2024-04-18 16:37:32 +0700

    Корректировка компонента Notification

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 import { formValidate } from './forms/formValidate'
 import { Sidebar } from './components/sidebar'
 import { loadJSON } from './products/productService'
+// import { Notification } from './components/notification'
 
 /**
  * Загружает данные при отрисовке страницы страницы.
@@ -19,3 +20,26 @@ const init = (() => {
  * @param {string} [position='left'] - Позиция сайдбара (по умолчанию 'left').
  */
 const panel = new Sidebar('#sidebar', '#show-sidebar', 'right')
+
+// Только для примера
+// const notification = new Notification({
+//   title: 'Добавление товара',
+//   subtitle: 'Товар добавлен на страницу',
+// })
+// const notification1 = new Notification({
+//   title: 'Добавление товара 1',
+//   subtitle: 'Товар добавлен на страницу 1',
+//   variant: 'warning',
+// })
+
+// const notification2 = new Notification({
+//   title: 'Добавление товара 2',
+//   subtitle: 'Товар добавлен на страницу 2',
+//   variant: 'error',
+// })
+
+// const notification3 = new Notification({
+//   title: 'Добавление товара 4',
+//   subtitle: 'Товар добавлен на страницу 4',
+//   variant: 'success',
+// })
